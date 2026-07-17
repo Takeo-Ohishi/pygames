e54d4784aff0a7908b02c737b5984ec889576897
Takeo Ohishi <takeo_ohishi_014@icloud.com>
2026-07-18 08:39:24 +0900

    2026/7/18 研修用に変更

--- moving6/main.py
+++ moving6/main.py
@@ -35,7 +35,6 @@ player_color = (0, 128, 255)  # 水色っぽい色
 clock = pygame.time.Clock()
 # ゲームを続けるかどうかのフラグ
 running = True
-is_dragging = False
 
 # ------------------ メインループ ------------------
 while running:
@@ -51,21 +50,7 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:  # 左クリック            if event.button == 1:  # 左クリック
-            # マウスの座標がキャラの四角形（rect）の中にあるか判定
-            if player_rect.collidepoint(event.pos):
-                is_dragging = True
-                # クリック位置とキャラの左上のズレを計算して保存
-                mouse_x, mouse_y = event.pos
-                offset_x, offset_y = player_rect.x - mouse_x, player_rect.y - mouse_y
-        elif event.type == pygame.MOUSEBUTTONUP and event.button == 1:  # 左ボタンが離されたらドラッグ終了
-            is_dragging = False
-
-    if is_dragging:
-        mouse_x, mouse_y = pygame.mouse.get_pos()
-        # ズレ（offset）を維持したまま移動させることで、ワープを防ぐ
-        player_rect.x, player_rect.y = mouse_x + offset_x, mouse_y + offset_y
-
+    # ------------------ マウス入力 ------------------
     # ------------------ 描画処理 ------------------
     screen.fill((0, 0, 0))  # 背景を黒で消す
     pygame.draw.rect(screen, player_color, player_rect) # プレイヤー（四角形）を描画します。引数は (画面, 色, 矩形)
